7d3ef6cd13fdbe12adb24d505ab18692379eff95
Sattawat <sa.khamsiri@gmail.com>
2021-05-14 14:21:24 +0700

    .

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Test_appman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "abc573";
+
+            string replacedqm = Regex.Replace(input, "[a-z]", " ");
+
+            Console.WriteLine(replacedqm.Replace(" ", String.Empty));
+
+            string input2 = "a5b7c3";
+
+            string replacedqm2 = Regex.Replace(input, "[a-z]", " ");
+
+            Console.WriteLine(replacedqm2.Replace(" ", String.Empty));
+
+        }
+    }
+}
