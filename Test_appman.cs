018c647d012aada8dd011b60b300aadbe4c13317
Sattawat <74730844+Sattawat-kham@users.noreply.github.com>
2021-05-17 14:50:41 +0700

    Update Test_appman.cs

--- Test_appman.cs
+++ Test_appman.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
